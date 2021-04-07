4918a13c826f1088670c9ff6a7ad16d77f45f3ea
Victor Doyle <victor-doyle@outlook.com>
2021-04-07 13:56:55 -0700

    star ratings for movies, serializer of ratings and two methods/ avg ratings / num of ratings added

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 # import mods
 from .models import Movie, Rating
+from django.contrib.auth.models import User
 from .serializers import MovieSerializer, RatingSerializer
 
 
@@ -15,9 +16,22 @@ class MovieViewSet(viewsets.ModelViewSet):
     def rate_movie(self, request, pk=None): 
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
-            print('movie title', movie.title)
-            response = {'message': 'rating submitted'}
-            return Response(response, status=status.HTTP_200_OK)
+            stars = request.data['stars']
+            # user = request.user
+            # hardcoded -> user = User.objects.get(id=1)
+            
+            try:
+                rating = Rating.objects.get(user = user.id, movie = movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False    )
+                response = {'message': 'Rating Updated', "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user= user, movie=movie, stars = stars )
+                serializer = RatingSerializer(rating, many=False    )
+                response = {'message': 'Rating Created', "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
         else: 
             response = {'message': 'You Need To Provide A Star Rating'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
